76ac5d0da8c8d4f4c8f11150c81b1d3482bb6076
Anuj Rajput <anujrajput04@gmail.com>
2021-01-28 15:30:01 +0530

    Added RounderCornerView

---
+++ JournalApp/RoundedCornerView.swift
@@ -0,0 +1,30 @@
+//
+//  RoundedCornerView.swift
+//  JournalApp
+//
+//  Created by Anuj Rajput on 28/01/21.
+//
+
+import UIKit
+
+class RoundedCornerView: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
